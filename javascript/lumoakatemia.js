41a49901a03d29063266cad4e8fdb11341867f68
Laura Knuuti <laura.knuuti@datalab.lan>
2024-09-09 14:21:03 +0300

    kommentti

--- javascript/lumoakatemia.js
+++ javascript/lumoakatemia.js
@@ -1,1 +1,44 @@
+/*Tehtävä 1*/
 console.log("hei maailma")
+
+/*Tehtävä2*/
+var l1, l2, summa;
+l1 = 3;
+l2 = 5;
+summa = l1 + l2;
+console.log(summa);
+
+/*Tehtävä 3*/
+var one, two, erotus;
+one = 10;
+two = 5;
+erotus = one - two
+console.log(erotus);
+
+/*Tehtävä 4*/
+var one, two, tulo;
+one = 3
+two = 4
+tulo = one * two
+console.log(tulo);
+
+/*Tehtävä 5*/
+var one, two, jako;
+one = 20
+two = 5
+jako = 20 / 5
+console.log(jako);
+
+/*Tehtävä 6*/
+var cel, fahr, tulo;
+cel = 10
+fahr = 1.8
+tulo = cel * fahr + 32
+console.log(tulo);
+
+/*Tehtävä 7*/
+var cel, fahr, jako;
+cel = 1,8
+fahr = 95
+jako = (95-32) / 1.8
+console.log(jako);
