8d0f22fd2bd8e70241847608df32656337aba2a5
laura.knuuti <laura.knuuti@gmail.com>
2024-10-02 13:59:29 +0300

    kommentti

---
+++ CSS/javascript/jstehtävä3.js
@@ -0,0 +1,9 @@
+function oddEven(n) {
+    document.write "Antamasi luku: " + n
+if (n & 1 == 1) {
+    return "Numero on pariton";
+}
+return "Numero on parillinen";
+}
+console.log (tarkistaLuku(12));
+console.log (tarkistaLuku(121));
